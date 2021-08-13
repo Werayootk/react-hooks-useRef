bd19f0c49a07d2600956887af898fbe711cc6231
Werayootk <werayoot5800@gmail.com>
2021-08-13 16:25:03 +0700

    case3 done

---
+++ src/useRefCase2/RefCase2.js
@@ -0,0 +1,32 @@
+import React, { useRef, useEffect, useState } from "react";
+
+const RefCase2 = () => {
+  const timerIdRef = useRef(0);
+  const [count, setCount] = useState(0);
+  
+  const startHandler = () => {
+    if (timerIdRef.current) { return; }
+    timerIdRef.current = setInterval(() => setCount(c => c+1), 1000);
+  };
+
+  const stopHandler = () => {
+    clearInterval(timerIdRef.current);
+    timerIdRef.current = 0;
+  };
+
+  useEffect(() => {
+    return () => clearInterval(timerIdRef.current);
+  }, []);
+    
+    return  (
+      <div>
+        <div>Timer: {count}s</div>
+        <div>
+          <button onClick={startHandler}>Start</button>
+          <button onClick={stopHandler}>Stop</button>
+        </div>
+      </div>
+    );
+};
+
+export default RefCase2;
